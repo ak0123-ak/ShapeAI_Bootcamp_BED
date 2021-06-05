acab40971860455b89e0b8aa6727642ade2b08b6
ANJANA A K <85388612+ak0123-ak@users.noreply.github.com>
2021-06-05 18:04:59 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Note()  {
+return (
+ <div className="note">
+   <h3>5 June World Environment Day</h3>
+   <p>World Environment Day (WED) is celebrated annually on 5 June 
+     and is the United Nations' principal vehicle for encouraging 
+     awareness and action for the protection of the environment. 
+     First held in 1974, it has been a platform for raising awareness 
+     on environmental issue such as marine pollution, human overpopulation,
+     global warming, sustainable consumption and wildlife crime. 
+     World Environment Day is a global platform for public outreach,
+     with participation from over 143 countries annually. Each year,
+     the program has provided a theme and forum for businesses,
+     non government organizations, communities, governments
+     and celebrities to advocate environmental causes.
+     </p>
+   </div>
+   );
+}
+
+export default Note;
